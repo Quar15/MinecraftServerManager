aedc2d6592a659de87af22c21c1f90884878c3a9
Kacper Janas <quar5@wp.pl>
2021-01-19 08:50:47 +0100

    ADDED: webscraper functions to get forge and mc versions

---
+++ webscraper.py
@@ -0,0 +1,34 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_versions():
+    URL = 'http://files.minecraftforge.net/'
+
+    headers = {
+        "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:68.0) Gecko/20100101 Firefox/68.0'}
+
+    page = requests.get(URL, headers=headers)
+
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+
+    forge_links = []
+    mc_versions = []
+    for url in soup.find_all("a"):
+        link = url.get('href')
+        if link and "/maven/net/minecraftforge/forge/index" in link:
+            mc_ver = link[38:-5]
+            mc_versions.append(mc_ver)
+            forge_link = "http://files.minecraftforge.net" + link
+            forge_links.append(forge_link)
+
+    return forge_links, mc_versions
+
+def get_forge_download_link(mc_ver, forge_links):
+    for forge_link in forge_links:
+        mc_ver_in_link = forge_link[69:-5]
+        if mc_ver == mc_ver_in_link:
+            return forge_link
+
+    return None
